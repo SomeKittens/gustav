0afac00039c83398194905655610f9accb400752
SomeKittens <rkoutnik@gmail.com>
2015-08-30 22:26:57 -0700

    update .d.ts, closes #7

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,14 @@
 declare module Gustav {
     class Node {
         constructor();
-        static dependencies(): void;
     }
     class Source extends Node {
     }
     class Transformer extends Node {
-        static dependencies(): void;
     }
-    class Loader extends Node {
-        static dependencies(): void;
+    class Sink extends Node {
     }
     function noop(): void;
     function init(...nodes: Array<Node>): void;
 }
-export = Gustav;
+export default Gustav;
