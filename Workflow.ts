c25c741c19684ac72ada3ac3a12afcc70d548c16
SomeKittens <rkoutnik@gmail.com>
2015-10-09 14:55:29 -0700

    fix(listener): re-add listener nodes when restarting a workflow

--- Workflow.ts
+++ Workflow.ts
@@ -19,27 +19,39 @@ export class Workflow {
   ggraph: GustavGraph;
   isStarted: boolean;
   guid: string;
+  listeners: NodeDef[];
+  private unsubs: any;
   constructor(public nodeDefs: NodeDef[]) {
     this.isStarted = false;
     this.guid = guid.new();
+    this.listeners = [];
+    this.init();
+  }
+  // Reset everything, destroying old references and saving memory
+  private init() {
+    this.isStarted = false;
+    this.unsubs = [];
+
     // array since the keys are numbers
     let idSymbolMap = [];
     // Add datas to GG
     this.ggraph = new GustavGraph();
+
     // Create a new node for each def
-    nodeDefs.forEach(def => {
+    this.nodeDefs.forEach(def => {
       let sym = gustav.makeNode(def.name, this.ggraph, def.config);
       idSymbolMap[def.id] = sym;
     });
 
     // add a dependencies based on ID
-    // TODO: This assumes that each node only has a single source of data
-    nodeDefs.forEach(def => {
-      if (!def.dataFrom) { return; }
-      this.ggraph.addEdge(idSymbolMap[def.id], idSymbolMap[def.dataFrom]);
+    this.nodeDefs.forEach(def => {
+      if (!def.dataFrom || !def.dataFrom.length) { return; }
+      def.dataFrom.forEach(from =>  this.ggraph.addEdge(idSymbolMap[def.id], idSymbolMap[from]));
     });
+
+    this.listeners.forEach(listener => this.addListener(listener));
   }
-  init() {
+  start() {
     this.isStarted = true;
     // traverse & run graph
     let cache = {};
@@ -85,11 +97,10 @@ export class Workflow {
 
     let listenerNode = gustav.makeNode(def.name, this.ggraph, def.config);
 
-    console.log(this.ggraph.sinkEdges);
-
     this.ggraph.sinkEdges
       .map(edge => edge.to)
-      .filter(sym => !console.log(sym))
       .forEach(sym => this.ggraph.addEdge(listenerNode, sym));
+
+    this.listeners.push(def);
   }
 }
