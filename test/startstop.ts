5b3782c047e17c805c5540e1e3dfc705442a9cdb
SomeKittens <rkoutnik@gmail.com>
2015-11-13 16:51:59 -0800

    chore(tests): Added test for start/stop

---
+++ test/startstop.ts
@@ -0,0 +1,41 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/chai/chai.d.ts" />
+
+'use strict';
+
+import {gustav} from '../index';
+import {INodeDef, Workflow} from '../Workflow';
+import {Observable} from '@reactivex/rxjs';
+import {expect} from 'chai';
+import {addCommonNodes} from './common';
+
+addCommonNodes(gustav);
+
+// Couple of common workflows
+let simpleWf:INodeDef[] = [{
+  id: 1,
+  name: 'intSource'
+}, {
+  id: 2,
+  name: 'fromIntSource',
+  dataFrom: 1
+}];
+
+// TODO: not stupid way of doing this
+describe('Workflow start/stop', () => {
+  it('should be able to start & stop a workflow', (done) => {
+    try {
+      let wf:Workflow = gustav.makeWorkflow(simpleWf);
+      wf.start();
+      setTimeout(() => {
+        wf.stop();
+        wf.start();
+        setTimeout(() => {
+          done();
+        }, 15);
+      }, 15);
+    } catch (e) {
+      done(e);
+    }
+  });
+});
