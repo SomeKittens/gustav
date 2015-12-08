9348f7b5eb0ec988a6c2355443872e6ca82226ec
SomeKittens <rkoutnik@gmail.com>
2015-12-07 17:53:12 -0800

    refactor: Gustav class -> POJO

--- index.ts
+++ index.ts
@@ -19,15 +19,13 @@ export interface IRegisteredNode {
   factory: Function;
 }
 
-export class __Gustav {
-  registeredNodes: IRegisteredNode[];
-  workflows: any;
-  constructor() {
-    this.registeredNodes = [];
-    this.workflows = {};
-  }
-  makeNode (nodeName: string, graph: GustavGraph, config: any): symbol {
-    let node = this.registeredNodes.filter((regNode) => regNode.name === nodeName)[0];
+let registeredNodes: IRegisteredNode[] = [];
+let workflows = {};
+let register;
+
+export let gustav = {
+  makeNode: (nodeName: string, graph: GustavGraph, config: any): symbol => {
+    let node = registeredNodes.filter((regNode) => regNode.name === nodeName)[0];
 
     if (!node) {
       throw new Error(nodeName + ' not registered');
@@ -45,52 +43,50 @@ export class __Gustav {
       init: config ? node.factory.bind(null, config) : node.factory
     };
     return sym;
-  }
+  },
   /**
    * Create an empty workflow to be chained off of
    * @param {string} uuid [description]
    */
   createWorkflow (name?: string): Workflow {
     let wf = new Workflow(name);
-    this.workflows[wf.uuid] = wf;
+    workflows[wf.uuid] = wf;
     return wf;
-  }
-  start (uuid: string): void {
-    this.workflows[uuid].start();
-  }
-  stop (uuid: string): void {
-    this.workflows[uuid].stop();
-  }
-  getNodeTypes (): INodeCollection {
-    return this.registeredNodes.reduce((obj, node) => {
+  },
+  start: (uuid: string): void => {
+    workflows[uuid].start();
+  },
+  stop: (uuid: string): void => {
+    workflows[uuid].stop();
+  },
+  getNodeTypes: (): INodeCollection => {
+    return registeredNodes.reduce((obj, node) => {
       obj[node.type].push(node.name);
       return obj;
     }, {source: [], transformer: [], sink: []});
-  }
-  source(name: string, factory: Function): Function { return this.register('source', name, factory); }
-  transformer(name: string, factory: Function): Function { return this.register('transformer', name, factory); }
-  sink(name: string, factory: Function): Function { return this.register('sink', name, factory); }
-
-  // TODO: new type of registration that's just a singleton
-  // Just calls INodeFactory and returns the symbol
-  private register(type: string, name: string, factory): INodeFactory {
-
-    // Names must be unique
-    const exists = this.registeredNodes.filter((regNode) => regNode.name === name);
-    if (exists.length) {
-      throw new Error(name + ' already registered');
-    }
-    this.registeredNodes.push({
-      type,
-      name,
-      factory
-    });
-
-    return this.makeNode.bind(this, name);
-  }
+  },
+  source(name: string, factory: Function): Function { return register('source', name, factory); },
+  transformer(name: string, factory: Function): Function { return register('transformer', name, factory); },
+  sink(name: string, factory: Function): Function { return register('sink', name, factory); },
 };
 
-export let gustav = new __Gustav();
+// TODO: new type of registration that's just a singleton
+// Just calls INodeFactory and returns the symbol
+register = (type: string, name: string, factory): INodeFactory => {
+
+  // Names must be unique
+  const exists = registeredNodes.filter((regNode) => regNode.name === name);
+  if (exists.length) {
+    throw new Error(name + ' already registered');
+  }
+  registeredNodes.push({
+    type,
+    name,
+    factory
+  });
+
+  return gustav.makeNode.bind(null, name);
+};
 
 // Meta nodes
 gustav.transformer('__gmergeNode', (nodes, iO) => {
