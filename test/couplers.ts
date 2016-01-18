21480c0dfcbbfd51d216e7b9dd1adab4e87d168d
SomeKittens <rkoutnik@gmail.com>
2016-01-18 15:03:44 -0800

    test(coupler): test multiple couplers

--- test/couplers.ts
+++ test/couplers.ts
@@ -27,6 +27,19 @@ describe('gustav.coupler', () => {
       .sink('fromIntTransformer', done);
   });
 
+  wfFactories.push((): Workflow => {
+    return gustav.createWorkflow('ex-2')
+      .from('mem', 'bill')
+      .transf('timesTwo')
+      .to('mem2', 'bob');
+  });
+
+  wfFactories.push((done): Workflow => {
+    return gustav.createWorkflow('ex-3')
+      .from('mem2', 'bob')
+      .sink('fromIntTransformer', done);
+  });
+
   it('allows for multiple workflows', (done) => {
     gustav.coupler(gm, 'mem');
 
@@ -38,6 +51,15 @@ describe('gustav.coupler', () => {
     gustav.coupler(gm);
 
     wfFactories[1](done).start();
+    wfFactories[0]().start();
+  });
+
+  it('allows for multiple couplers', done => {
+    gustav.coupler(gm);
+    gustav.coupler(new GustavMem(), 'mem2');
+
+    wfFactories[3](done).start();
+    wfFactories[2]().start();
     wfFactories[0]().start();
   });
 
