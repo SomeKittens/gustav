306b7669f05c6fd9c0bfa2bb1b141662cbe3905c
SomeKittens <rkoutnik@gmail.com>
2016-01-18 13:39:29 -0800

    feat(rabbit): added rabbit coupler

---
+++ external/GustavRabbit.ts
@@ -0,0 +1,69 @@
+'use strict';
+
+import {Observable, Subscription} from '@reactivex/rxjs';
+import {IExternalClient} from '../defs';
+import {connect} from 'amqplib';
+import * as uuid from 'node-uuid';
+
+// Untested, use at own risk
+
+// Uses fanout exchanges so we can connect many queues to them
+export class GustavRabbit implements IExternalClient {
+  constructor(public config?: any) {
+    this.config = this.config || {
+      connString: 'amqp://localhost'
+    };
+  }
+
+  from(exchange: string): Observable<any> {
+    let queue = uuid.v4();
+    return new Observable(o => {
+      let conn;
+      connect(this.config.connString)
+      .then(c => conn = c && c.createChannel())
+      .then(ch => {
+        ch.assertExchange(exchange, 'fanout', {durable: true});
+        ch.assertQueue(queue, {durable: true});
+
+        ch.bindQueue(queue, exchange, '');
+        ch.consume(queue, msg => {
+          o.next(msg.content.toString());
+          ch.ack(msg);
+        }, {noAck: false});
+      })
+      .catch(err => o.error(err));
+
+      return () => conn && conn.close();
+    });
+  }
+
+  to(exchange: string, iO: Observable<any>): Subscription<any> {
+    let channel, conn, cachedItems = [];
+    let queue = uuid.v4();
+
+    connect(this.config.connString)
+    .then(c => conn = c && c.createChannel())
+    .then(c => {
+      channel = c;
+
+      channel.assertExchange(exchange, 'fanout');
+      cachedItems.forEach(item => {
+        channel.publish(exchange, '', new Buffer(item));
+      });
+    })
+    .catch(err => { throw err; });
+
+    return iO
+      .subscribe(
+        msg => {
+          if (!channel) {
+            return cachedItems.push(msg);
+          }
+
+          channel.publish(exchange, '', new Buffer(msg));
+        },
+        err => console.error(`rabbitSink err, queue: ${queue}`, err),
+        () => conn && conn.close()
+      );
+  }
+}
